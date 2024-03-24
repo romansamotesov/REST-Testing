db12cd4795234073ae615122525e8b816ef1f41a
Roman Samotesov <RomanSamotesov@coherentsolutions.com>
2024-03-24 10:44:55 +0100

    finished_task2

--- TestApi/MyUnitTest.cs
+++ TestApi/MyUnitTest.cs
@@ -1,5 +1,7 @@
 using RestSharp;
 using REST_Testing;
+using System.Net;
+using System.Threading.Tasks.Dataflow;
 
 
 namespace TestApi
@@ -12,8 +14,16 @@ namespace TestApi
         }
 
         [Test]
-        public void Authentification()
+        public void GetZipCodes_Test()
         {
+            var expectedZipCodes = new List<string>()
+            {
+                "12345",
+                "23456",
+                "ABCDE",
+                "12333",
+                "12344"
+            };
 
             var options = new RestClientOptions()
             {
@@ -21,7 +31,86 @@ namespace TestApi
             };
             var client = new RestClient(options);
             var request = new RestRequest("http://localhost:49000/zip-codes");
-            var result = client.GetAsync(request).Result;
+            var response = client.GetAsync(request).Result;
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Status Code is not 200");
+            var receivedZipcodes = ZipCode.ZipCodesToList(response.Content);
+            Assert.That(receivedZipcodes, Is.EqualTo(expectedZipCodes), "list of zipCodes is not equal");
+        }
+
+        [Test]
+        public void PostZipCodes_Test()
+        {
+            var expectedZipCodes = new List<string>()
+            {
+                "12345",
+                "23456",
+                "ABCDE",
+                "12333",
+                "12344"
+            };
+
+            var options = new RestClientOptions()
+            {
+                Authenticator = WriteAuthenticator.getInstance(),
+            };
+            var client = new RestClient(options);
+            var request = new RestRequest("http://localhost:49000/zip-codes/expand");;
+            var zipcodes = new ZipCode(new List<string>() { "12333", "12344" });
+            request.AddJsonBody(zipcodes.Body);
+            var response = client.Post(request);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created), "Status Code is not 201");
+            var receivedZipcodes = ZipCode.ZipCodesToList(response.Content);
+            Assert.That(receivedZipcodes, Is.EqualTo(expectedZipCodes), "lists of zipCodes are not equal");
+        }
+
+        [Test]
+        public void PostDuplicateZipCodes_Test()
+        {
+            var expectedZipCodes = new List<string>()
+            {
+                "12345",
+                "23456",
+                "ABCDE",
+                "12333"
+            };
+
+            var options = new RestClientOptions()
+            {
+                Authenticator = WriteAuthenticator.getInstance(),
+            };
+            var client = new RestClient(options);
+            var request = new RestRequest("http://localhost:49000/zip-codes/expand"); ;
+            var zipcodes = new ZipCode(new List<string>() { "12333", "12333" });
+            request.AddJsonBody(zipcodes.Body);
+            var response = client.Post(request);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created), "Status Code is not 201");
+            var receivedZipcodes = ZipCode.ZipCodesToList(response.Content);
+            Assert.That(receivedZipcodes, Is.EqualTo(expectedZipCodes), "lists of zipCodes are not equal");
+        }
+
+        [Test]
+        public void PostAlreadyUsedZipCodes_Test()
+        {
+            var expectedZipCodes = new List<string>()
+            {
+                "12345",
+                "23456",
+                "ABCDE",
+                "12333"
+            };
+
+            var options = new RestClientOptions()
+            {
+                Authenticator = WriteAuthenticator.getInstance(),
+            };
+            var client = new RestClient(options);
+            var request = new RestRequest("http://localhost:49000/zip-codes/expand"); ;
+            var zipcodes = new ZipCode(new List<string>() { "12333", "12345" });
+            request.AddJsonBody(zipcodes.Body);
+            var response = client.Post(request);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created), "Status Code is not 201");
+            var receivedZipcodes = ZipCode.ZipCodesToList(response.Content);
+            Assert.That(receivedZipcodes, Is.EqualTo(expectedZipCodes), "lists of zipCodes are not equal");
         }
     }
 }
