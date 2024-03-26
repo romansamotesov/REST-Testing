e22a98aa8069fa934827bc5539293228109f38d7
Roman Samotesov <RomanSamotesov@coherentsolutions.com>
2024-03-26 10:03:57 +0100

    Finished_task3

---
+++ REST-Testing/UserResponse.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace REST_Testing
+{
+    public record class UserResponse
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("age")]
+        public int Age { get; set; }
+        [JsonPropertyName("sex")]
+        public string Sex { get; set; }
+        [JsonPropertyName("zipCode")]
+        public string ZipCode { get; set; }
+    }
+}
--- TestApi/MyUnitTest.cs
+++ TestApi/MyUnitTest.cs
@@ -110,5 +110,103 @@ namespace TestApi
             var receivedZipcodes = ZipCode.ZipCodesToList(response.Content);
             Assert.That(receivedZipcodes, Is.EqualTo(expectedZipCodes), "lists of zipCodes are not equal");
         }
+
+        [Test]
+        public void PostCreateUser_Test()
+        {
+            var expectedZipCodes = new List<string>()
+            {
+                "12345",
+                "ABCDE"
+            };
+
+            var options = new RestClientOptions()
+            {
+                Authenticator = WriteAuthenticator.getInstance(),
+            };
+            var writeClient = new RestClient(options);
+            var createUserRequest = new RestRequest("http://localhost:49000/users");
+            var user = new User(20, "TestName", Enums.Sex.FEMALE, "23456");
+            createUserRequest.AddJsonBody(user);
+            var createUserResponse = writeClient.Post(createUserRequest);
+            Assert.That(createUserResponse.StatusCode, Is.EqualTo(HttpStatusCode.Created), "Status Code is not 201");
+
+            options.Authenticator = ReadAuthenticator.getInstance();
+            var readClient = new RestClient(options);
+            var getUserRequest = new RestRequest("http://localhost:49000/users");
+            var getUserResponse = readClient.GetAsync<List<UserResponse>>(getUserRequest).Result;
+            Assert.That(getUserResponse.Any(u => u.Name.Equals(user.Name)), "User is not created");
+
+            var getZipCodesRequest = new RestRequest("http://localhost:49000/zip-codes");
+            var getZipCodesResponse = readClient.GetAsync(getZipCodesRequest).Result;
+            Assert.That(getZipCodesResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Status Code is not 200");
+            var receivedZipcodes = ZipCode.ZipCodesToList(getZipCodesResponse.Content);
+            Assert.That(receivedZipcodes, Is.EqualTo(expectedZipCodes), "list of zipCodes is not equal");
+        }
+
+        [Test]
+        public void PostRequiredFieldsCreateUser_Test()
+        {
+            var options = new RestClientOptions()
+            {
+                Authenticator = WriteAuthenticator.getInstance(),
+            };
+            var writeClient = new RestClient(options);
+            var createUserRequest = new RestRequest("http://localhost:49000/users");
+            var user = new User("TestNameRF", Enums.Sex.FEMALE);
+            createUserRequest.AddJsonBody(user);
+            var createUserResponse = writeClient.Post(createUserRequest);
+            Assert.That(createUserResponse.StatusCode, Is.EqualTo(HttpStatusCode.Created), "Status Code is not 201");
+
+            options.Authenticator = ReadAuthenticator.getInstance();
+            var readClient = new RestClient(options);
+            var getUserRequest = new RestRequest("http://localhost:49000/users");
+            var getUserResponse = readClient.GetAsync<List<UserResponse>>(getUserRequest).Result;
+            Assert.That(getUserResponse.Any(u => u.Name.Equals(user.Name)), "User is not created");
+        }
+
+        [Test]
+        public void PostNotAvailableZipCodeCreateUser_UserIsNotCreated_Test()
+        {
+            var options = new RestClientOptions()
+            {
+                Authenticator = WriteAuthenticator.getInstance(),
+            };
+            var writeClient = new RestClient(options);
+            var createUserRequest = new RestRequest("http://localhost:49000/users");
+            var user = new User(20, "TestName", Enums.Sex.FEMALE, "111");
+            createUserRequest.AddJsonBody(user);
+            var exception = Assert.Throws<HttpRequestException>(() => writeClient.Post(createUserRequest));
+            Assert.That(exception.StatusCode, Is.EqualTo(HttpStatusCode.FailedDependency), "Status Code is not 424");
+
+            options.Authenticator = ReadAuthenticator.getInstance();
+            var readClient = new RestClient(options);
+            var getUserRequest = new RestRequest("http://localhost:49000/users");
+            var getUserResponse = readClient.GetAsync<List<UserResponse>>(getUserRequest).Result;
+            Assert.That(getUserResponse.Any(u => u.Name.Equals(user.Name)), Is.False, "User is created");
+        }
+
+        [Test]
+        public void PostDuplicateCreateUser_UserIsNotCreated_Test()
+        {
+            var options = new RestClientOptions()
+            {
+                Authenticator = WriteAuthenticator.getInstance(),
+            };
+            var writeClient = new RestClient(options);
+            var createUserRequest = new RestRequest("http://localhost:49000/users");
+            var user = new User(20, "TestName", Enums.Sex.FEMALE, "23456"); ;
+            createUserRequest.AddJsonBody(user);
+            var createUserResponse = writeClient.Post(createUserRequest);
+            Assert.That(createUserResponse.StatusCode, Is.EqualTo(HttpStatusCode.Created), "Status Code is not 201");
+            var exception = Assert.Throws<HttpRequestException>(() => writeClient.Post(createUserRequest));
+            Assert.That(exception.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest), "Status Code is not 400");
+
+            options.Authenticator = ReadAuthenticator.getInstance();
+            var readClient = new RestClient(options);
+            var getUserRequest = new RestRequest("http://localhost:49000/users");
+            var getUserResponse = readClient.GetAsync<List<UserResponse>>(getUserRequest).Result;
+            Assert.That(getUserResponse.Any(u => u.Name.Equals(user.Name)), Is.False, "User is created");
+        }
     }
 }
