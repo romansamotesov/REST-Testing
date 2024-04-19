2936caf9a36b5579277652a9a6247620ae646b48
Roman Samotesov <RomanSamotesov@coherentsolutions.com>
2024-04-19 09:36:48 +0200

    stash_changes

--- TestApi/MyUnitTest.cs
+++ TestApi/MyUnitTest.cs
@@ -2,15 +2,10 @@ using RestSharp;
 using REST_Testing;
 using System.Net;
 using System.Threading.Tasks.Dataflow;
-using System.Reflection.PortableExecutable;
 using NUnit.Allure.Core;
 using NUnit.Allure.Attributes;
-using NUnit.Allure;
-using NUnit.Framework.Constraints;
 using Allure.Net.Commons;
 using System.Text;
-using System.Reflection.Emit;
-using System.Linq;
 using Newtonsoft.Json;
 
 
